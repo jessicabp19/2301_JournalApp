5e7f73d162b72513ad2902f26c14431c79db9390
Jessica <jessicaboton@gmail.com>
2023-01-17 18:50:03 -0600

    ADD Components & Views

--- journal-app/src/journal/layout/JournalLayout.jsx
+++ journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,22 +1,24 @@
+import { Toolbar } from "@mui/material";
 import { Box } from "@mui/system"
+import { NavBar, SideBar } from "../components";
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 //Layout { children}
 export const JournalLayout = ({children}) => {
   return (
     <Box sx={{ display: 'flex'}}>
 
-        {/* Navbar drawerWidth */}
+        <NavBar drawerWidth={drawerWidth} />
 
-        {/* Sidebar drawerWidth */}
+        <SideBar drawerWidth={drawerWidth } />
 
         <Box 
             component='main'
             sx={{ flexGrow: 1, p: 3 }}
         >
 
-            {/* Toolbar */}
+            <Toolbar />
 
             { children }
 
--- journal-app/src/journal/pages/JournalPage.jsx
+++ journal-app/src/journal/pages/JournalPage.jsx
@@ -1,13 +1,31 @@
-import { Typography } from "@mui/material"
+import { IconButton } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from '../view'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Veniam pariatur adipisicing sunt aute enim. Commodo consectetur anim do dolor deserunt eu laboris eu elit sint ullamco labore ipsum. Anim cupidatat voluptate velit aliqua irure adipisicing sint adipisicing non id irure. Officia cupidatat labore deserunt elit amet duis laborum incididunt sint veniam minim ad cillum. Cupidatat elit quis consectetur sit voluptate. Est duis consequat incididunt enim fugiat sunt reprehenderit incididunt consequat id nostrud. Et nostrud anim et id.</Typography>
+      {/* <Typography>Veniam pariatur adipisicing sunt aute enim. Commodo consectetur anim do dolor deserunt eu laboris eu elit sint ullamco labore ipsum. Anim cupidatat voluptate velit aliqua irure adipisicing sint adipisicing non id irure. Officia cupidatat labore deserunt elit amet duis laborum incididunt sint veniam minim ad cillum. Cupidatat elit quis consectetur sit voluptate. Est duis consequat incididunt enim fugiat sunt reprehenderit incididunt consequat id nostrud. Et nostrud anim et id.</Typography> */}
     
-      {/* NothingSelected */}
-      {/* NoteView */}
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.8},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+
+      </IconButton>
 
     </JournalLayout>
   )
